1c9ca491d0dff434f2dacbcd4e781ea8c3c84489
Justin-Orr <jj.oreo98@gmail.com>
2021-11-22 09:53:18 -0600

    Update starting button background style and code cleanup

--- Carousel (Image Slider)/script.js
+++ Carousel (Image Slider)/script.js
@@ -1,4 +1,4 @@
-var counter = 1;
+var counter = 2;
 setInterval(function(){
   document.getElementById('radio' + counter).checked = true;
 
@@ -13,7 +13,7 @@ setInterval(function(){
   }
 }, 5000);
 
-var first_slide = document.getElementById("first");
+var carousel_image = document.getElementById("carousel_image");
 
 var r1 = document.getElementById("radio1").addEventListener("click", slide_transition1);
 var r2 = document.getElementById("radio2").addEventListener("click", slide_transition2);
@@ -26,33 +26,25 @@ var l3 = document.getElementById("rl3");
 var l4 = document.getElementById("rl4");
 
 function slide_transition1() {
-  console.log("Button 1 pressed");
-  first_slide.style.transition = "margin-left 1.5s";
-  first_slide.style.marginLeft = "0%";
+  carousel_image.style.backgroundImage = "url(image-bike-repair-service-b.jpg)";
   select_label_1()
   counter = 1;
 }
 
 function slide_transition2() {
-  console.log("Button 2 pressed");
-  first_slide.style.transition = "margin-left 1.5s";
-  first_slide.style.marginLeft = "-100%";
+  carousel_image.style.backgroundImage = "url(instagram_profile_image.png)";
   select_label_2()
   counter = 2;
 }
 
 function slide_transition3() {
-  console.log("Button 3 pressed");
-  first_slide.style.transition = "margin-left 1.5s";
-  first_slide.style.marginLeft = "-200%";
+  carousel_image.style.backgroundImage = "url(image-bike-repair-service-b.jpg)";
   select_label_3()
   counter = 3;
 }
 
 function slide_transition4() {
-  console.log("Button 4 pressed");
-  first_slide.style.transition = "margin-left 1.5s";
-  first_slide.style.marginLeft = "-300%";
+  carousel_image.style.backgroundImage = "url(instagram_profile_image.png)";
   select_label_4()
   counter = 4;
 }
